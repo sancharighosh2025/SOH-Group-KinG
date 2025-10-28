a21bf084230bdfce0b1522ed5d4a2b7f0144e60c
Sanchari Ghosh <gsanchari9870@gmail.com>
2025-10-28 23:14:34 +0530

    update

--- server/dist/configs/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const serverless_1 = require("@neondatabase/serverless");
-const sql = (0, serverless_1.neon)(`${process.env.DATABASE_URL}`);
-exports.default = sql;
